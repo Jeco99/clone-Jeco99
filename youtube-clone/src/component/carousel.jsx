da332c418f03263b85e2cf25ab851709ca9b8d1f
Jdeo Barrera <jdeo.barrera@gmail.com>
2023-06-16 22:07:51 +0800

    youtube-clone

--- youtube-clone/src/component/carousel.jsx
+++ youtube-clone/src/component/carousel.jsx
@@ -10,22 +10,68 @@ export default function PillsTabs() {
     >
       <Tabs.Item
         active
-        title="Tab 1"
+        title="All"
       >
      
       </Tabs.Item>
 
-      <Tabs.Item title="Tab 2">
+      <Tabs.Item title="Live">
      
       </Tabs.Item>
 
-      <Tabs.Item title="Tab 3">
+      <Tabs.Item title="Music">
       
       </Tabs.Item>
 
-      <Tabs.Item title="Tab 4">
+      <Tabs.Item title="Gaming">
        
       </Tabs.Item>
+
+      <Tabs.Item title="News">
+       
+      </Tabs.Item>
+
+      <Tabs.Item title="Gameshow">
+      
+      </Tabs.Item>
+
+      <Tabs.Item title="Philippine Music">
+       
+      </Tabs.Item>
+
+      <Tabs.Item title="Playlist">
+       
+      </Tabs.Item>
+
+      <Tabs.Item title="Greatest hits album">
+      
+      </Tabs.Item>
+
+      <Tabs.Item title="Basketball">
+       
+      </Tabs.Item>
+
+      <Tabs.Item title="K-pop">
+       
+      </Tabs.Item>
+
+      <Tabs.Item title="Engines">
+       
+      </Tabs.Item>
+
+      <Tabs.Item title="Tools">
+      
+      </Tabs.Item>
+
+      <Tabs.Item title="Agriculture">
+       
+      </Tabs.Item>
+
+      <Tabs.Item title="Computers">
+       
+      </Tabs.Item>
+
+      
 
       <Tabs.Item
         disabled
