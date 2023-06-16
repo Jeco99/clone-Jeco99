12f711b96fe1ddf22cf47d2f5933b934adec5c14
Jeco99 <jerriccojohncabunagan00@gmail.com>
2023-06-16 22:56:32 +0800

    change to thumbnail image

--- youtube-clone/src/component/displayVideo.jsx
+++ youtube-clone/src/component/displayVideo.jsx
@@ -2,86 +2,88 @@
 import { Card } from "flowbite-react";
 import PillsTabs from "./carousel";
 
+import thumbNail from '../assets/images/lightboxexample1.png'
+
 const cardItems = [
   {
-      video: 'https://www.youtube.com/watch?v=9Ns8DztSfwo',
+      video:  thumbNail,
       title: 'SB19 \'ILAW\' Lyric Video',
       accountName: 'SB19 Official', 
       noOfViews: '335K views',
       numberofDays: '6 days ago'
   }, 
   {
-      video: 'https://www.youtube.com/watch?v=9Ns8DztSfwo',
+    video:  thumbNail,
       title: 'SB19 \'ILAW\' Lyric Video',
       accountName: 'SB19 Official', 
       noOfViews: '335K views',
       numberofDays: '6 days ago'
   },
   {
-    video: 'https://www.youtube.com/watch?v=9Ns8DztSfwo',
+    video:  thumbNail,
     title: 'SB19 \'ILAW\' Lyric Video',
     accountName: 'SB19 Official', 
     noOfViews: '335K views',
     numberofDays: '6 days ago'
   },
   {
-    video: 'https://www.youtube.com/watch?v=9Ns8DztSfwo',
+    video:  thumbNail,
     title: 'SB19 \'ILAW\' Lyric Video',
     accountName: 'SB19 Official', 
     noOfViews: '335K views',
     numberofDays: '6 days ago'
   },
   {
-    video: 'https://www.youtube.com/watch?v=9Ns8DztSfwo',
+    video:  thumbNail,
     title: 'SB19 \'ILAW\' Lyric Video',
     accountName: 'SB19 Official', 
     noOfViews: '335K views',
     numberofDays: '6 days ago'
   },
   {
-    video: 'https://www.youtube.com/watch?v=9Ns8DztSfwo',
+    video:  thumbNail,
     title: 'SB19 \'ILAW\' Lyric Video',
     accountName: 'SB19 Official', 
     noOfViews: '335K views',
     numberofDays: '6 days ago'
   },
   {
-    video: 'https://www.youtube.com/watch?v=9Ns8DztSfwo',
+    video:  thumbNail,
     title: 'SB19 \'ILAW\' Lyric Video',
     accountName: 'SB19 Official', 
     noOfViews: '335K views',
     numberofDays: '6 days ago'
   },
   {
-    video: 'https://www.youtube.com/watch?v=9Ns8DztSfwo',
+    video:  thumbNail,
     title: 'SB19 \'ILAW\' Lyric Video',
     accountName: 'SB19 Official', 
     noOfViews: '335K views',
     numberofDays: '6 days ago'
   },
   {
-    video: 'https://www.youtube.com/watch?v=9Ns8DztSfwo',
+    video:  thumbNail,
     title: 'SB19 \'ILAW\' Lyric Video',
     accountName: 'SB19 Official', 
     noOfViews: '335K views',
     numberofDays: '6 days ago'
   },
   {
-    video: 'https://www.youtube.com/watch?v=9Ns8DztSfwo',
+    video:  thumbNail,
     title: 'SB19 \'ILAW\' Lyric Video',
     accountName: 'SB19 Official', 
     noOfViews: '335K views',
     numberofDays: '6 days ago'
   },
   {
-    video: 'https://www.youtube.com/watch?v=9Ns8DztSfwo',
+    video:  thumbNail,
     title: 'SB19 \'ILAW\' Lyric Video',
     accountName: 'SB19 Official', 
     noOfViews: '335K views',
     numberofDays: '6 days ago'
   },
   {
-    video: 'https://www.youtube.com/watch?v=9Ns8DztSfwo',
+    video:  thumbNail,
     title: 'SB19 \'ILAW\' Lyric Video',
     accountName: 'SB19 Official', 
     noOfViews: '335K views',
@@ -91,23 +93,21 @@ const cardItems = [
 
 
 function DisplayVideo() {
-  const videoWidth = '250'
+  
+
   return (
     <>
         <PillsTabs />
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-5">
         {cardItems.map( ({video, title, accountName, noOfViews, numberofDays}) => (
           <Card key={title}>
-          <video controls width={videoWidth}>
-            <source src={video} type="" />
-          </video>
-          <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">
+          <img src={video} alt={title} />
+          <h5 className="mb-2 text-2xl font-bold text-gray-900 dark:text-white">
             {title}
           </h5>
           <div>
             <p>{accountName}</p>
-            <p>{noOfViews}</p>
-            <p>{numberofDays}</p>
+            <p>{noOfViews} &#x2022; {numberofDays}</p>
           </div>
         </Card>
         ))}
